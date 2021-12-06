ccaf54f5506c5b20e7e74e72ac685e351b9db498
HIMANSHU SINGH <71926216+developerhimanshu@users.noreply.github.com>
2021-12-06 09:56:54 +0530

    Add files via upload

---
+++ practical 11/task1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class A{
+public:
+int x;
+int y;
+
+};
+class B: public A
+{
+public:
+void display()
+{
+x=10;
+y=20;
+cout<<x<<" "<<y<<endl;
+}
+
+};
+int main()
+{
+B obj;
+obj.x=30;
+obj.y=40;
+obj.display();
+
+return 0;
+}
